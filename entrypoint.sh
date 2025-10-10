512a1124f103773cc2f7b88344ecd6e07bfd50e6
John Agyemang <oseiagyemanjohn@gmail.com>
2025-10-10 15:33:49 +0000

    Nginx  solved

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
+# #!/usr/bin/env bash
+# set -e
+
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+
+# exec "$@"
+
+
 #!/usr/bin/env bash
 set -e
 
-# Wait for Redis (and DB if using TCP) as needed
-# Example for Postgres via TCP (skip if using Neon over Internet and it's reliable):
-# until nc -z -w3 db 5432; do echo "Waiting for DB..."; sleep 2; done
-
-python manage.py collectstatic --noinput
+# Run migrations
 python manage.py migrate --noinput
+
+# Collect static files only in production
+if [ "$ENV" = "production" ]; then
+    python manage.py collectstatic --noinput
+fi
 
 exec "$@"
