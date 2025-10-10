6e91b5714f498c3fe9e176eb6767a3c8b77d9e84
John Agyemang <oseiagyemanjohn@gmail.com>
2025-10-10 16:09:18 +0000

    Nginx --solved-2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,9 @@
-# #!/usr/bin/env bash
-# set -e
-
-# python manage.py collectstatic --noinput
-# python manage.py migrate --noinput
-
-# exec "$@"
-
-
 #!/usr/bin/env bash
 set -e
 
-# Run migrations
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-# Collect static files only in production
-if [ "$ENV" = "production" ]; then
-    python manage.py collectstatic --noinput
-fi
-
 exec "$@"
+
+
