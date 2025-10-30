25634950c915227eec236afb1100669cabb3f17a
John Agyemang <oseiagyemanjohn@gmail.com>
2025-10-30 12:58:56 +0000

    COOKIEs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -386,7 +386,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Vite dev
     "http://localhost:3000",  # Next.js dev
     "http://127.0.0.1:3000",
     "https://negromart.com",
